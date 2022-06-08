3c995eb16487c407cf03c67e4655f69e1445157b
Gabriel Machado <gramchado12@hotmail.com>
2022-06-08 00:02:41 -0400

    Initial functions

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,20 +4,29 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.less']
+  styleUrls: ['./game.component.less'],
 })
 export class GameComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
+  attack() {
+    //TODO Implementar ataque
+  }
 
-  giveUp(){
-    if(confirm("Tem certeza que deseja desistir?")){
+  expecialAttack() {
+    //TODO Implementar ataque Especial
+  }
+
+  healing() {
+    //TODO Implementar cura
+  }
+
+  giveUp() {
+    if (confirm('Tem certeza que deseja desistir?')) {
       // TODO: implementar desistir
       this.router.navigateByUrl('home');
     }
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
